e92fb2fa09ad2bd583af3ff607bbc2ff417d6b04
Emmanuel-9 <oregadaniel181@gmail.com>
2021-02-22 15:36:06 +0300

    Add the category and likes field to the class Post.

--- users/models.py
+++ users/models.py
@@ -1,11 +1,31 @@
 from django.db import models
+import datetime as dt
+from django.urls import reverse
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(blank=True)
 
-class User(models.Model):
+class UserProfile(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField() 
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = 'posts/')
+    content = models.TextField(blank=True)
+    writer = models.ForeignKey('UserProfile', on_delete=models.CASCADE,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+    likes = models.ManyToManyField(UserProfile, related_name='post_writer')
+    category = models.CharField(max_length=50, default='coding')
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def total_likes():
+        return self.likes.count()
+        
+
+    
+    
+    
+
